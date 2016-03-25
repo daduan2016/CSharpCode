263b94ec90821e247606781951f1caba69431cd8
daduan2016 <duanmuwang_2006@163.com>
2016-03-26 01:50:58 +0800

    第一次提交

---
+++ FirstNet/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FirstNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.txtLabel = new System.Windows.Forms.Label();
+            this.btnPing = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(243, 65);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(219, 21);
+            this.txtIp.TabIndex = 0;
+            // 
+            // txtLabel
+            // 
+            this.txtLabel.AutoSize = true;
+            this.txtLabel.Location = new System.Drawing.Point(309, 138);
+            this.txtLabel.Name = "txtLabel";
+            this.txtLabel.Size = new System.Drawing.Size(0, 12);
+            this.txtLabel.TabIndex = 1;
+            this.txtLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnPing
+            // 
+            this.btnPing.Location = new System.Drawing.Point(494, 65);
+            this.btnPing.Name = "btnPing";
+            this.btnPing.Size = new System.Drawing.Size(70, 28);
+            this.btnPing.TabIndex = 2;
+            this.btnPing.Text = "Ping";
+            this.btnPing.UseVisualStyleBackColor = true;
+            this.btnPing.Click += new System.EventHandler(this.btnPing_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 261);
+            this.Controls.Add(this.btnPing);
+            this.Controls.Add(this.txtLabel);
+            this.Controls.Add(this.txtIp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label txtLabel;
+        private System.Windows.Forms.Button btnPing;
+    }
+}
+
---
+++ FirstNet/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstNet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPing_Click(object sender, EventArgs e)
+        {
+            Ping ping = new Ping();
+            PingReply reply = ping.Send(txtIp.Text);
+            if( reply.Status == IPStatus.Success)
+            {
+                txtLabel.Text = string.Format("ip{0}测试成功！！", txtIp.Text);
+                MessageBox.Show("测试成功");
+            }
+        }
+    }
+}
